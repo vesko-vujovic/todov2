8ac411875ed9b6694e54392a95861aae7b6b5e84
Vesko Vujovic <veskovujovic@yahoo.com>
2015-04-27 12:31:12 +0200

    small changes
    some small changes

---
+++ todo.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    var msg        = $('#danger').hide();
+    var inputText  = $('#todo');
+    var list       = $('#list');
+
+
+   function check(input)
+   {
+       this.inputVal     = input;
+       this.message      = msg;
+       this.arrayOfObj   = [];
+       this.obj
+
+   }
+
+
+
+});
