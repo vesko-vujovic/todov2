50c0d8ab8f1f46f48c2bf98dd17d1f725a50651e
Vesko Vujovic <veskovujovic@yahoo.com>
2015-04-27 14:44:02 +0200

    one more object to go
    one more function and im done

--- todo.js
+++ todo.js
@@ -1,35 +1,57 @@
 $(document).ready(function(){
     var msg        = $('#danger').hide();
     var list       = $('#list');
+    var input      = $('#todo');
+    var arrayOfObj = [];
 
-   function Check(e)
-   {   
-       this.inputVal        =  $('#todo').val();
-       this.message         = msg;
-       this.arrayOfObj      = [];
+    function Check()
+   {
        this.obj;
-       this.isEmpty         = function(){
-           this.inputVal == '' ? this.message.show(): this.createObject(), this.message.hide(), this.inputVal = '';
+       this.isEmpty         = function(input){
+          if(input === '')
+          { msg.show()}
+          else
+          {
+            msg.hide();
+            this.createObject(input);
+          }
        };
-       this.createObject    = function(){
-           obj        = new Object();
-           obj.name   = "input";
-           obj.value  = '' + this.inputVal + '';
-           this.addObjectToArray(obj);
-           console.log(obj);
+       this.createObject    = function(input){
+           this.obj        = new Object();
+           this.obj.name   = "input";
+           this.obj.value  = '' + input + '';
+           this.addObjectToArray(this.obj);
        };
        this.addObjectToArray = function(obj){
-           this.arrayOfObj.push(obj);
-           console.log(arrayOfObj);
+           arrayOfObj.push(obj);
        }
 
    }
 
+   //object for conversion and adding to cookie
+   function Convert()
+   {
+       this.convert;
+       this.prepare      = function(){
+          this.convert   = JSON.stringify(arrayOfObj);
+          this.addToCookie(this.convert);
+       },
+       this.addToCookie  = function(convertedArray){
+           $.cookie('todo2', convertedArray);
+       }
+
+
+   }
+
+
+
 
     $('#add').on('click', function (event) {
         event.preventDefault();
-        var  obj1 = new Check();
-             obj1.isEmpty();
+        var  obj1    = new Check();
+        obj1.isEmpty(input.val());
+
+        console.log(dataArr);
     });
 
 
