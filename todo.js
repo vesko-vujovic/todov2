f94fb7ec9d2768b1f026856529a858ed3ff4852e
Vesko Vujovic <veskovujovic@yahoo.com>
2015-04-27 14:03:31 +0200

    changes
    smalll changes

--- todo.js
+++ todo.js
@@ -1,23 +1,36 @@
 $(document).ready(function(){
     var msg        = $('#danger').hide();
-    var inputText  = $('#todo');
     var list       = $('#list');
 
-
-   function check(input)
-   {
-       this.inputVal     = input;
-       this.message      = msg;
-       this.arrayOfObj   = [];
+   function Check(e)
+   {   
+       this.inputVal        =  $('#todo').val();
+       this.message         = msg;
+       this.arrayOfObj      = [];
        this.obj;
-       this.isEmpty      = function(){
-           this.inputVal == ''? msg.show(): create
+       this.isEmpty         = function(){
+           this.inputVal == '' ? this.message.show(): this.createObject(), this.message.hide(), this.inputVal = '';
        };
-       this.createObject  = {name: 'input', value: ''+ this.inputVal  +''}
-
+       this.createObject    = function(){
+           obj        = new Object();
+           obj.name   = "input";
+           obj.value  = '' + this.inputVal + '';
+           this.addObjectToArray(obj);
+           console.log(obj);
+       };
+       this.addObjectToArray = function(obj){
+           this.arrayOfObj.push(obj);
+           console.log(arrayOfObj);
+       }
 
    }
 
+
+    $('#add').on('click', function (event) {
+        event.preventDefault();
+        var  obj1 = new Check();
+             obj1.isEmpty();
+    });
 
 
 });
